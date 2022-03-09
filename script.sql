60dc7e5d87ec183e14c186cbc958b30d6482fbf9
Hmmsien <98779692+Hmmsien@users.noreply.github.com>
2022-03-09 11:04:40 -0500

    Update script.sql

--- script.sql
+++ script.sql
@@ -1,21 +1,22 @@
-DROP TABLE IF EXISTS Judges;
-DROP TABLE IF EXISTS Car_Score;
-DROP TABLE IF EXISTS Cars_Info;
-DROP TABLE IF EXISTS Rank;
-DROP TABLE IF EXISTS Ranking;
+sqlite3 cars.db << 'EOS'
+.headers on
+.mode csv
 
+DROP TABLE IF EXISTS Judges;
 CREATE TABLE Judges (
     Judge_ID text,
     Judge_Name text);
 .mode csv
 .import \Judges.csv Judges
 
+DROP TABLE IF EXISTS Car_Score;
 CREATE TABLE Car_Score(
    Car_ID int primary key,
    Car_Score int);
 .mode csv
 .import \Car_Score.csv Car_Score
 
+DROP TABLE IF EXISTS Cars_Info;
 CREATE TABLE Cars_Info(
    Car_ID int primary key,
    Year int,
@@ -26,6 +27,7 @@ CREATE TABLE Cars_Info(
 .mode csv
 .import \Cars_Info.csv Cars_Info
 
+DROP TABLE IF EXISTS Ranking;
 CREATE TABLE Ranking(
    Car_ID int primary key,
    Year int,
@@ -37,6 +39,7 @@ From Cars_Info
 INNER JOIN Car_Score ON Car_Score.Car_ID = Cars_Info.Car_ID
 ORDER BY  Car_Score.Car_Score DESC;
 
+DROP TABLE IF EXISTS Rank;
 CREATE TABLE Rank(
    Ranking int,
    Car_ID int primary key,
@@ -49,3 +52,7 @@ INSERT INTO Rank (Ranking, Car_ID, Year, Make, Model) SELECT rowid,Car_ID,Year,M
 .output extract1.csv
 SELECT * From Rank;
 
+.mode column
+.output
+
+EOS
