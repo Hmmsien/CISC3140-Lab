5f1d16f703b03ba5c2a2f60f7671dbeccd30be33
Huimin.Chen23 <hmyyqxi@gmail.com>
2022-03-08 23:44:12 -0500

    update extract1

--- script.sql
+++ script.sql
@@ -2,6 +2,7 @@ DROP TABLE IF EXISTS Judges;
 DROP TABLE IF EXISTS Car_Score;
 DROP TABLE IF EXISTS Cars_Info;
 DROP TABLE IF EXISTS Rank;
+DROP TABLE IF EXISTS Ranking;
 
 CREATE TABLE Judges (
     Judge_ID text,
@@ -25,18 +26,22 @@ CREATE TABLE Cars_Info(
 .mode csv
 .import \Cars_Info.csv Cars_Info
 
+CREATE TABLE Ranking(
+   Car_ID primary key,
+   Car_Score int);
+
+INSERT INTO Ranking SELECT * FROM Car_Score Order By Car_Score DESC;
+
 CREATE TABLE Rank(
    Rank int,
-   Car_ID int,
-   Year int,
-   Make text,
-   Model text);
+   Car_ID primary key);
 
-INSERT INTO Rank (Rank, Car_ID, Year, Make, Model) 
-SELECT Car_Score.rowid,Cars_Info.Car_ID,Cars_Info.Year,Cars_Info.Make,Cars_Info.Model 
-FROM Car_Score INNER JOIN Cars_Info ON Cars_Info.Car_ID = Car_Score.Car_ID
-ORDER BY Car_Score.Car_Score DESC;
+INSERT INTO Rank SELECT rowid,Car_ID FROM Ranking;
 
 .mode csv
 .output extract1.csv
-SELECT * FROM Rank; 
+SELECT Rank.Rank, Rank.Car_ID, Cars_Info.Year,Cars_Info.Make,Cars_Info.Model
+From Rank
+INNER JOIN Cars_Info on Cars_Info.Car_ID = Rank.Car_ID;
+
+
